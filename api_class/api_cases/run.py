e5d2e0d1d9592f056758d1de3a95110f3c72cc12
liqilong <liqilong@wondersgroup.com>
2021-08-23 09:58:45 +0800

    接口自动化

--- api_class/api_cases/run.py
+++ api_class/api_cases/run.py
@@ -5,3 +5,11 @@ if __name__ == "__main__":
     runner = HTMLTestRunner.HTMLTestRunner(stream=result,title="接口测试报告",description="执行测试情况")
     runner.run(discover)
     result.close()
+
+
+if __name__ == "__main__":
+    discover = unittest.defaultTestLoader.discover(start_dir="./",pattern="api_case.py")
+    result = open("./api_report.html","wb")
+    runner = HTMLTestRunner.HTMLTestRunner(stream=result,title="接口测试报告",description="执行测试情况")
+    runner.run(discover)
+    result.close()
